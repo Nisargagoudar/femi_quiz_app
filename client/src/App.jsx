1ab068564191de26ef20eee1c3edf3a129e575e0
Nisargagoudar <nisargasgoudar@gmail.com>
2024-08-20 14:18:02 +0530

    about and contact page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import {BrowserRouter  as Router, Routes, Route} from "react-router-dom";
 import AdminLogin from './pages/admin_pages/AdminLogin';
 import Homepage from './pages/common_pages/Homepage';
+import About from "./pages/common_pages/About";
+import Contact from "./pages/common_pages/Contact";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +17,8 @@ function App() {
           <Route path="/" element = {<Homepage />}></Route>
           <Route path="/home" element = {<Homepage />}></Route>
           <Route path="/homepage" element = {<Homepage />}></Route>
+          <Route path="/about" element = {<About />}></Route>
+          <Route path="/contact" element = {<Contact />}></Route>
           <Route path="/admin-login" element = {<AdminLogin />}></Route>
        </Routes>
     </Router>
