052737bed7647038f3eabdd78db247d0480fc870
Nisargagoudar <nisargasgoudar@gmail.com>
2024-08-20 14:25:40 +0530

    header updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter  as Router, Routes, Route} from "react-router-dom";
 import AdminLogin from './pages/admin_pages/AdminLogin';
 import Homepage from './pages/common_pages/Homepage';
 import About from "./pages/common_pages/About";
 import Contact from "./pages/common_pages/Contact";
+import Header from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Router>
+      <Header />
        <Routes>
           <Route path="/" element = {<Homepage />}></Route>
           <Route path="/home" element = {<Homepage />}></Route>
